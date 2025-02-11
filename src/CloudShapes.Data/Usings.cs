45fb1f07aba3aaf6726851cbeaa12d85f172ab70
Charles d'Avernas <charles.davernas@neuroglia.io>
2025-02-11 15:50:18 +0100

    fix(Data): Fixed wierd `Properties` name regression in the `ProjectionIndexDefinition` model
    Signed-off-by: Charles d'Avernas <charles.davernas@neuroglia.io>

--- src/CloudShapes.Data/Usings.cs
+++ src/CloudShapes.Data/Usings.cs
@@ -21,6 +21,7 @@ global using Neuroglia.Eventing.CloudEvents;
 global using Neuroglia.Serialization;
 global using System;
 global using System.Collections.Generic;
+global using System.ComponentModel.DataAnnotations;
 global using System.Text.Json;
 global using System.Text.Json.Serialization;
 global using System.Text.RegularExpressions;
