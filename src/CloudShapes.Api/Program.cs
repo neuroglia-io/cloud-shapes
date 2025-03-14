e749811ea12f7f6c6c0c04e7d8e3cd76c9574f03
Charles d'Avernas <charles.davernas@neuroglia.io>
2025-03-14 11:02:54 +0100

    fix(Api): Removed artifacts from the Program.cs file
    Signed-off-by: Charles d'Avernas <charles.davernas@neuroglia.io>

--- src/CloudShapes.Api/Program.cs
+++ src/CloudShapes.Api/Program.cs
@@ -11,7 +11,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-using Json.Schema;
 using MongoDB.Bson;
 using MongoDB.Bson.Serialization.Serializers;
 
